0296a0f43cc34389f1fc6ac359a86a8cc2f8d85d
Luis <78446571+V3RSER@users.noreply.github.com>
2026-09-10 00:19:26 -0500

    refactor

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,10 @@
-import { AuthView } from '@/components/AuthView';
+import { AuthView } from '@/components/login/AuthView';
 
 export default async function LoginPage({
     searchParams,
-}: {
+}: Readonly<{
     searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
-}) {
+}>) {
     const resolvedSearchParams = await searchParams;
     const error = typeof resolvedSearchParams.error === 'string' ? resolvedSearchParams.error : undefined;
 
--- app/page.tsx
+++ app/page.tsx
@@ -18,3 +18,4 @@ export default async function Page({
 
     redirect('/dashboard');
 }
+z
